59585b9e11c4029cf4a5348dbac7d6328d1ecb85
Jhen Nuñez <107572992+JxElektro@users.noreply.github.com>
2022-11-19 16:41:50 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+let arr = [
+  Jhen,
+  32,
+  true,
+  [31, 5, 1990],
+  ,{
+    name: 'El Padrino',
+    author: 'Mario Puzo',
+    year: 1969,
+    url: 'https://www.amazon.com/El-Padrino-Mario-Puzo/dp/0451191148'
+  }
+]
